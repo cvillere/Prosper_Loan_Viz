ae45934370fd9efe67827d2809a91b4a33ff3346
Christian Villere <christianvillere@gmail.com>
2017-03-27 21:50:51 -0700

    DimpleJS first draft code New dataset in Consum_vs_Spec.csv; New data wrangling code in Prosper_Data_Wrangling.R, and the DimpleJS code is in Untitled2.

--- Project_Files/Prosper_Data_Wrangling.R
+++ Project_Files/Prosper_Data_Wrangling.R
@@ -3,6 +3,8 @@ getwd()
 library(plyr)
 detach(package:plyr)
 library(dplyr)
+library(ggplot2)
+#install.packages(ggplot)
 
 setwd("/Users/christianvillere/DandProjects/Data_Viz/Final_Project/Project_Files")
 Prosper_Data <- read.csv("prosperLoanData.csv")
@@ -35,6 +37,58 @@ Grouped.Totals <- Working_Data %>%
             Avg_Inc = median(StatedMonthlyIncome),
             Avg_Loan = median(LoanOriginalAmount),
             Count_Category_Num = n())
+
+
+?by
+
+by(Grouped.Totals, Grouped.Totals["ListingCategory..numeric."], summary)
+
+#Move the new column to beginning of dataframe
+Prosper_Data <- Prosper_Data[,c(1:2,82,3:81)]
+
+
+subset_data <- subset(Grouped.Totals, 
+    Grouped.Totals["ListingCategory..numeric."] == 5, 15, 16, 17, 18, 19, 20)
+
+
+p1 <- ggplot(aes(x = CreationYear, y = total_loan), data = subset_data) +
+  geom_point() + geom_line(group = 1)
+
+
+#1)real big ramp up for category 1(debt consolidation) from 09-13, before a dip.
+#2)same story for cat. 2(Home improvement) as cat. 1
+#3)spiked from 07 to 08. Diped to 09. Rose steadily to a spike in 12. Diped a 
+#little in 13. Diped big time from 13 to 14. cat.3(business)
+#4) cate. 4(personal), big increase from 07 to 08, but no other data exists
+#except for those years.
+#5)huge increase from 07 to 08. huge decline from 08 to 09. no movement 
+#from 09 to 10. cate.5(student)
+#6)rises from 07 to 08. falls from 08 to 09. rises for 09 to 12. Falls from 
+#12 to 14. cate. 6(auto)
+#7) is other category. so not exploring
+#8) huge increase from 11 to 13, before a decrease from 13 to 14. 
+# cate. 8(baby and adoption)
+#9) flat line from 12 to 13, before it craters from 13 to 14. cate. 9 (Boat).
+#10) only info for 12 and 13. drops 60% from 12 to 13. cate. 10(cosmetic)
+#11) huge increase from 11 to 12. subsequent drops from 12 to 14. only info
+#for 11 to 14. cate. 11(engagement ring)
+#12) huge increase from 11 to 12. small increase from 12 to 13. steep drop
+#from 13 to 14. cate. 12(green loans)
+#13) huge increase from 11 to 12. small decreases from 12 to 13 and 13 to 14.
+#cate. 13(household expenses)
+#14) huge increase from 11 to 12 and 12 to 13. pretty big drop from 13 to 14
+#cate. 14(large purchases)
+#15) huge increase from 11 to 12. Slight decrease from 12 to 13. Big 
+#dropoff from 13 to 14. cate. 15(medical/dental)
+#16) huge increase from 11 to 12. decrease from 12 to 13 and 13 to 14. 
+#cate. 16(motorcycle)
+#17) starts high in 12. Decreases from 12 to 13 and 13 to 14. cate. 17(RV)
+#18) shoots up from 11 to 12. stays flat from 12 to 13. decreases 
+#significantly from 13 to 14. cate. 18(Taxes)
+#19) shoots up significantly from 11 to 12, before subsiding from 12 to 13 and 
+#13 to 14. cate. 19(vacation)
+#20) shoots up siginificantly from 11 to 12, before subsiding from 12 to 13 and
+#13 to 14. cate. 20(wedding)
 
 write.table(Grouped.Totals, file='Grouped_Totals.csv', quote=FALSE, sep=',', 
             row.names = F)
